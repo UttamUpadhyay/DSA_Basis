6cdd77b120b24e280f5580cbb2c0e6d381fb83f0
UttamUpadhyay <upadhyayuttam197@gmail.com>
2026-07-29 10:31:55 +0530

    sizeofopt

---
+++ OOPS/sizeofopt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    cout << "Size of datatype int -: " << sizeof(int) << endl;
+    cout << "Size of datatype float -:" << sizeof(float) << endl;
+    cout << "Size of datatype char -:" << sizeof(char) << endl;
+    cout << "Size of datatype double -:" << sizeof(double) << endl;
+    cout << "Size of datatype long -:" << sizeof(long) << endl;
+}
