7b534961d1a5e1e979f27c8a9d721b94790f3feb
UttamUpadhyay <upadhyayuttam197@gmail.com>
2026-08-20 14:47:06 +0530

    Queue using linkedlist by struct

--- Queue/QueueUsingLinkedlist.cpp
+++ Queue/QueueUsingLinkedlist.cpp
@@ -9,7 +9,57 @@ struct node{
 
 node *GetNode()
 {
-    node * p;
-    p =(node *) malloc(sizeof(struct node));
+    node *p; 
+    p = new node;
     return p;
+}
+node *rear, *front;
+ 
+void EnQueue(int x)
+{
+    node *temp;
+    temp = GetNode();
+    temp -> data = x;
+    temp -> next = NULL;
+    if (rear != NULL)
+    {
+        rear-> next = temp;
+    }
+    else {
+        front = temp;
+    }
+    rear = temp;
+}
+
+int DeQueue()
+{
+    node *p = front;
+    front = front -> next;
+    int x = p->data;
+    delete(p);
+    return x;
+}
+void Initialize() 
+{
+    rear = NULL;
+    front = NULL;
+}
+
+int main() 
+{
+    Initialize();
+    EnQueue(1);
+    EnQueue(2);
+    EnQueue(3);
+    EnQueue(4);
+    EnQueue(5);
+    EnQueue(6);
+    cout << DeQueue() << endl;
+    cout << DeQueue() << endl;
+    cout << DeQueue() << endl;
+    cout << DeQueue() << endl;
+    cout << DeQueue() << endl;
+    cout << DeQueue() << endl;
+
+
 }
